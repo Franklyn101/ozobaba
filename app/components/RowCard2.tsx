864edca9fabf60a71058345c0fb9e4819aa445a6
Franklyn inoma <125371243+Franklyn101@users.noreply.github.com>
2024-05-31 03:40:40 -0700

    Update RowCard2.tsx

--- app/components/RowCard2.tsx
+++ app/components/RowCard2.tsx
@@ -12,7 +12,7 @@ export default function RowCard2({}: Props) {
       <div className=" w-full grid grid-cols-1 md:grid-cols-2 gap-8">
         <CardNoShadow
           img={ImageCard}
-          caption="Lorem ipsum dolor sit amet consectetur. Tellus pulvinar cras sed posuere duis. Velit euismod quis sed ut quis. Elit sit facilisis viverra facilisi adipiscing. Tortor pellentesque est dictumst pellentesque amet vitae faucibus.Lorem ipsum dolor sit amet consectetur. Tellus pulvinar cras sed posuere duis. Velit euismod quis sed ut quis. Elit sit facilisis viverra facilisi adipiscing. Tortor pellentesque est dictumst pellentesque amet vitae faucibus.Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Tellus pulvinar cras sed posuere duis. Velit euismod quis sed ut quis. Elit sit  "
+          caption="It describes a strategy focused on improving processes, increasing quality, and cutting waste.To acheive continous delivery, a business should apply te continous improvement mindset"
           title="Child-Centric Approach"
         />
         <CardNoShadow
